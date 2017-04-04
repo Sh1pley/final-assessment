cea4b0ee4d91919aad2f4da4ac2ff42e970f95a4
Jesse Shipley <jesse.radack@gmail.com>
2017-04-04 13:33:59 -0600

    adds code to pass log out and changing links for signed in users

--- spec/features/authenticated_user_spec.rb
+++ spec/features/authenticated_user_spec.rb
@@ -15,7 +15,7 @@ describe "Authenticated User" do
     expect(page).to have_content("Come on in")
   end
 
-    scenario "as a registered user I should see a log out button and not a sign in button" do
+  scenario "as a registered user I should see a log out button and not a sign in button" do
     visit login_path
     fill_in :session_email, with: @user.email
     fill_in :session_password, with: @user.password
@@ -24,7 +24,22 @@ describe "Authenticated User" do
 
     expect(current_path).to eq(root_path)
     expect(page).to have_content("Come on in")
-    expect(page).to not_have_link("Sign Up")
+    expect(page).to_not have_link("Sign Up")
     expect(page).to have_link("Logout")
+  end
+
+  scenario "after loggin out I should see a way to log back in" do
+    visit login_path
+    fill_in :session_email, with: @user.email
+    fill_in :session_password, with: @user.password
+    fill_in :session_password_confirmation, with: @user.password
+    click_on "Log in"
+
+    expect(current_path).to eq(root_path)
+    expect(page).to have_content("Come on in")
+
+    click_on "Logout"
+
+    expect(page).to have_content("Please Log in or Register")
   end
 end
