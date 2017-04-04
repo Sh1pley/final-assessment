c0e2493c05a7c687e58a4554202307daa3c652fb
Jesse Shipley <jesse.radack@gmail.com>
2017-04-04 15:52:42 -0600

    fell into pitfall with validates url, found a better way

--- spec/features/submitting_links_spec.rb
+++ spec/features/submitting_links_spec.rb
@@ -12,8 +12,8 @@ describe "as a User, when logged in" do
     click_on "Log in"
 
     expect(current_path).to eq('/')
-    expect(page).to have_field(:link_title)
-    expect(page).to have_field(:link_url)
+    expect(page).to have_field(:title)
+    expect(page).to have_field(:url)
   end
 
   scenario "should be able to submit new links and see them" do
@@ -22,9 +22,10 @@ describe "as a User, when logged in" do
     fill_in :session_password, with: @user.password
     fill_in :session_password_confirmation, with: @user.password
     click_on "Log in"
-    
-    fill_in :link_title, with: "Super Nice Link"
-    fill_in :link_url, with: "https://turing.io"
+    save_and_open_page
+    fill_in :title, with: "Super Nice Link"
+    fill_in :url, with: "https://turing.io"
+    click_on "Add Link"
 
     link = Link.first
 
