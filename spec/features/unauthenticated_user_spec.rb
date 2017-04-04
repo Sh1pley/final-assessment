d9db93b89918993a0eb7c2470b234ce5c37c0ace
Jesse Shipley <jesse.radack@gmail.com>
2017-04-04 10:44:38 -0600

    add tests for create new user

--- spec/features/unauthenticated_user_spec.rb
+++ spec/features/unauthenticated_user_spec.rb
@@ -5,6 +5,16 @@ describe "Unauthenticated User" do
     visit '/'
 
     expect(page).to have_content("Please Log in")
-    expect(page).to have_link("Register")
+    expect(page).to have_link("Sign Up")
+  end
+
+  scenario "click on Sign Up should take you to a new user form" do
+    visit '/'
+    click_on "Sign Up"
+
+    expect(current_path).to eq(new_user_path)
+    expect(page).to have_form("Email Address")
+    expect(page).to have_form("Password")
+    expect(page).to have_form("Password Confirmation")
   end
 end
