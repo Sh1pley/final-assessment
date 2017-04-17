d3b1bb50d53877df02e30ec75f5623a877d47ef9
Jesse Shipley <jesse.radack@gmail.com>
2017-04-17 09:26:04 -0600

    remove password conf from regular log in

--- spec/features/mark_as_read_spec.rb
+++ spec/features/mark_as_read_spec.rb
@@ -1,9 +1,17 @@
 require "rails_helper"
 
 RSpec.describe "can mark links as read", :js => :true do
-  xscenario "Mark a link as read" do
-    Link.create(url:"https://turing.io", title:"Turing")
-    visit "/"
+  before do
+    @user = User.create(email: "jesse@j.com", password: "1234")
+    link = Link.create(url:"https://turing.io", title:"Turing")
+    @user.links << link
+  end
+
+  scenario "Mark a link as read" do
+    visit login_path
+    fill_in :session_email, with: @user.email
+    fill_in :session_password, with: @user.password
+    click_on "Log in"
     within('.link .read-status') do
       expect(page).to have_text("false")
     end
