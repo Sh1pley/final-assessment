70b98ea5b4414b3bc6d5f1d1cb4b3444bc5d69ba
Jesse Shipley <jesse.radack@gmail.com>
2017-04-17 10:09:41 -0600

    adds additional tests for editing links

--- spec/features/editing_links_spec.rb
+++ spec/features/editing_links_spec.rb
@@ -4,17 +4,35 @@ describe 'Editing Links' do
   before do
     @user = User.create(email: "test", password: "1111")
     @user.links << Link.create(title: "test link", url: "https://testlink.com")
-  end
-
-  scenario 'clicking on Edit will take you to a page to edit the link' do
     visit root_path
     fill_in :session_email, with: @user.email
     fill_in :session_password, with: @user.password
     click_on "Log in"
+  end
 
+  scenario 'clicking on Edit will take you to a page to edit the link' do
     click_on "Edit"
 
     expect(current_path).to eq('/links/1/edit')
-    
+  end
+
+  scenario 'changing the title of the link, will update the title' do
+    click_on "Edit"
+    fill_in :title, with: "not a test link"
+    click_on "Update Link"
+    link = Link.first
+
+    expect(current_path).to eq(root_path)
+    expect(link.title).to eq("not a test link")
+  end
+
+  scenario 'changint the url of the link, updates the url' do
+    click_on "Edit"
+    fill_in :url, with: "https://not.test.com"
+    click_on "Update Link"
+    link = Link.first
+
+    expect(current_path).to eq(root_path)
+    expect(link.url).to eq("https://not.test.com")
   end
 end
