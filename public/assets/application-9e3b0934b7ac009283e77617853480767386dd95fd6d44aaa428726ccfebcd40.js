a5a8151cbc77e20b0d3185a51e1e337e9dc6d2a6
Jesse Shipley <jesse.radack@gmail.com>
2017-04-17 14:27:59 -0600

    recompiled assets

--- public/assets/application-9e3b0934b7ac009283e77617853480767386dd95fd6d44aaa428726ccfebcd40.js
+++ public/assets/application-9e3b0934b7ac009283e77617853480767386dd95fd6d44aaa428726ccfebcd40.js
@@ -11647,6 +11647,30 @@ function displayFailure(failureData){
   console.log("FAILED attempt to update Link: " + failureData.responseText);
 }
 ;
+$(document).ready( () => {
+  filterLinks();
+})
+
+filterLinks = () => {
+  $('input[name=search]').keyup( () => {
+    let $rows = $('.link');
+    let $search = $('input[name=search]').val().toLowerCase();
+    $rows.hide();
+    showRows($search, $rows);
+  })
+}
+
+showRows = ($search, $rows) => {
+  for (var i = 0; i < $rows.length; i++) {
+    let title = $($rows[i]).children().children('p.link-title').text().toLowerCase();
+    let url = $($rows[i]).children().children('p.link-url').text().toLowerCase();
+    if ((title.indexOf($search) >= 0) || (url.indexOf($search) >= 0)) {
+      $($rows[i]).show();
+    }
+    
+  }
+}
+;
 // This is a manifest file that'll be compiled into application.js, which will include all the files
 // listed below.
 //
