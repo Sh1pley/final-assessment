db5fd4150aa26b75df33980f7f25d50fa9210b74
Jesse Shipley <jesse.radack@gmail.com>
2017-04-17 15:25:27 -0600

    js functionality has broken older tests, working on fixes

--- spec/features/submitting_links_spec.rb
+++ spec/features/submitting_links_spec.rb
@@ -1,34 +1,30 @@
 require 'rails_helper'
 
-describe "as a User, when logged in" do
-  before do
+describe "as a User, when logged in", :js => :true do
+
+  it "should see a form to submit links" do
     @user = User.create(email: "test@testy.com", password: "1111")
-  end
-  scenario "should see a form to submit links" do
     visit root_path
     fill_in :session_email, with: @user.email
     fill_in :session_password, with: @user.password
     click_on "Log in"
-
     expect(current_path).to eq('/')
     expect(page).to have_field(:title)
     expect(page).to have_field(:url)
   end
 
-  scenario "should be able to submit new links and see them" do
+  it "should be able to submit new links and see them" do
+    @user = User.create(email: "test@testy.com", password: "1111")
     visit root_path
     fill_in :session_email, with: @user.email
     fill_in :session_password, with: @user.password
     click_on "Log in"
     fill_in :title, with: "Super Nice Link"
     fill_in :url, with: "https://turing.io"
+    save_and_open_page
     click_on "Add Link"
 
-    link = Link.first
-    
-    expect(link.title).to eq("Super Nice Link")
-    expect(link.url).to eq("https://turing.io")
-    expect(page).to have_content("Super Nice Link")
     expect(page).to have_css("a", text: "turing.io..")
+    expect(page).to have_css("alert", text: "Super Nice Link added.")
   end
 end
