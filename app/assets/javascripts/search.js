decadd07552262a5080e97916d0f1e3eea18c5a0
Jesse Shipley <jesse.radack@gmail.com>
2017-04-17 15:54:50 -0600

    adds toggle for show/hide of links by read status

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -1,5 +1,7 @@
 $(document).ready( () => {
   filterLinks();
+  unreadFilter();
+  readFilter();
 })
 
 filterLinks = () => {
@@ -18,6 +20,19 @@ showRows = ($search, $rows) => {
     if ((title.indexOf($search) >= 0) || (url.indexOf($search) >= 0)) {
       $($rows[i]).show();
     }
-    
   }
+}
+
+unreadFilter = () => {
+  $('.unread-filter').click( () => {
+    let $read = $('.true')
+    $read.toggle() // could use .hide() for one way hiding
+  })
+}
+
+readFilter = () => {
+  $('.read-filter').click( () => {
+    let $unread = $('.false')
+    $unread.toggle() // same as above
+  })
 }
